effada3d2c20c51185ee81e27c40e27ffb7da5a3
Vine <2282458988@qq.com>
2022-12-27 22:42:21 +0800

    chore: update swift version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.6
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -18,7 +18,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/swift-onedrive/microsoft-graph-core.git", from: "0.0.2")
+        .package(url: "https://github.com/swift-onedrive/microsoft-graph-core.git", from: "0.0.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
